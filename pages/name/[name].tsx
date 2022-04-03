deb6ff17532c92c6ab13864c7b166bb96d1bab42
Leonardo Valeriano <leonardo.valeriano.2000@gmail.com>
2022-04-03 13:54:51 -0500

    fin de la sección 6

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -22,7 +22,7 @@ const PokemonByNamePage: NextPage<Props> = ({ pokemon }) => {
 		localFavorites.toggleFavorites(pokemon.id)
 		setIsInFavorites(!isInFavorites)
 
-		if (!isInFavorites) return
+		if (isInFavorites) return
 
 		confetti({
 			zIndex: 999,
@@ -127,13 +127,21 @@ export const getStaticPaths: GetStaticPaths = async ctx => {
 		paths: pokemonNames.map(name => ({
 			params: { name },
 		})),
-		fallback: false,
+		fallback: "blocking",
 	}
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
 	const { name } = params as { name: string }
 	const pokemon = await getPokemonInfo(name)
+	if (!pokemon) {
+		return {
+			redirect: {
+				destination: "/",
+				permanent: false,
+			},
+		}
+	}
 	return {
 		props: {
 			pokemon,
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -8,7 +8,7 @@ import confetti from "canvas-confetti"
 import { pokeApi } from "../../api"
 import { Layout } from "../../components/layout"
 import { Pokemon } from "../../interfaces"
-import { localFavorites } from "../../utils"
+import { getPokemonInfo, localFavorites } from "../../utils"
 
 interface Props {
 	pokemon: Pokemon
@@ -120,17 +120,26 @@ export const getStaticPaths: GetStaticPaths = async ctx => {
 		paths: pokemon151.map(id => ({
 			params: { id },
 		})),
-		fallback: false,
+		fallback: "blocking",
 	}
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
 	const { id } = params as { id: string }
-	const { data } = await pokeApi.get<Pokemon>(`/pokemon/${id}`)
+	const pokemon = await getPokemonInfo(id)
+	if (!pokemon) {
+		return {
+			redirect: {
+				destination: "/",
+				permanent: false,
+			},
+		}
+	}
 	return {
 		props: {
-			pokemon: data,
+			pokemon,
 		},
+		revalidate: 86400,
 	}
 }
 
